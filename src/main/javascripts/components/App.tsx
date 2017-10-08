017ea31d1ab4b61668687a6a25f339e28e26cf1a
Kevin Liu <kevin@potatofrom.space>
2017-10-08 14:59:21 -0400

    Add basic styling

--- src/main/javascripts/components/App.tsx
+++ src/main/javascripts/components/App.tsx
@@ -4,8 +4,8 @@ import LeakTable from './LeakTable';
 export default class App extends React.PureComponent {
     render() {
         return (
-            <div>
-                <h1>Leaks</h1>
+            <div className="app">
+                <h1 className="app__header">Leaks</h1>
                 <LeakTable />
             </div>
         );
