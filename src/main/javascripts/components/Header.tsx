6267fcf873e2c878d690e88202632803ce83708e
Kevin Liu <kevin@potatofrom.space>
2017-10-08 21:36:06 -0400

    Improve header

--- src/main/javascripts/components/Header.tsx
+++ src/main/javascripts/components/Header.tsx
@@ -3,7 +3,8 @@ import * as React from 'react';
 export default function Header() {
     return (
         <header className="header">
-            <h1>Gasleaks.info</h1>
+            <h1 className="header__siteName">Gasleaks.info</h1>
+            <p className="header__slogan">Mapping Acton's hidden gas leaks</p>
         </header>
     );
 }
