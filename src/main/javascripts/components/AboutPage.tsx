2e98c51d443920a24206babf33a971e54a318b24
Kevin Liu <kevin@potatofrom.space>
2017-10-08 20:56:24 -0400

    Replace about page with placeholder

--- src/main/javascripts/components/AboutPage.tsx
+++ src/main/javascripts/components/AboutPage.tsx
@@ -3,10 +3,7 @@ import * as React from 'react';
 export default class AboutPage extends React.PureComponent {
     render() {
         return (
-            // TODO better message!
-            <footer className="footer">
-                Made by Kevin Liu, Kunal Sharda, and Aneesh Edara for Resource Force.
-            </footer>
+            <p>PLACEHOLDER</p>
         );
     }
 }
