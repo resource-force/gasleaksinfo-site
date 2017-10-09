05e580fc14fc1cf1d470fad4ca824960a04b6d44
Kevin Liu <kevin@potatofrom.space>
2017-10-09 08:48:44 -0400

    Fix HTTP 500 on /api/r0/leaks/# due to stack overflow

--- src/main/kotlin/info/gasleaks/site/data/Leak.kt
+++ src/main/kotlin/info/gasleaks/site/data/Leak.kt
@@ -36,4 +36,12 @@ data class Leak(
         val status: LeakStatus,
         val size: Int?,
         val reportedOn: LocalDate?,
-        val fixedOn: LocalDate?)
+        val fixedOn: LocalDate?) {
+    // HACK to avoid recursive loop of calling toString on dataset which calls
+    // toString on Leak etc. For some reason, Spring/JPA calls this when you
+    // navigate to /api/r0/leaks/#, even though it outputs Leak as serialized
+    // JSON so toString doesn't really do anything.
+    override fun toString(): String {
+        return ""
+    }
+}
