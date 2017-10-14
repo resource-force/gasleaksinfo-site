478139f31c952f14bb19fbab40590ea1d01b2420
Kevin Liu <kevin@potatofrom.space>
2017-10-14 10:01:03 -0400

    Clean up getLeaks function with async and await

--- src/main/javascripts/helpers/client.ts
+++ src/main/javascripts/helpers/client.ts
@@ -3,18 +3,11 @@ import Leak from "../data/Leak";
 import api from "../helpers/api";
 
 export async function getLeaks() {
-    return api("/api/r0/leaks")
-        // Get JSON response
-        .then((res) => res.entity._embedded.leaks)
-        // Set href to leak reference
-        .then((leaks) => {
-            leaks.forEach((leak) => leak.href = new URL(leak._links.self.href));
-            return leaks;
-        })
-        // Set source via another fetch request
-        .then((leaks) => Promise.all(leaks.map(async (leak: any) =>
-            fetch(leak._links.source.href)
-                .then((res) => res.json())
-                .then((source) => { leak.source = source; return leak; }),
-            )));
+    const response = await api("/api/r0/leaks");
+    return await Promise.all(response.entity._embedded.leaks
+        .map((leak) => Object.assign(leak, { href: new URL(leak._links.self.href) }))
+        .map(async (leak) => {
+            const source = await api(leak._links.source.href);
+            return Object.assign(leak, { source: source.entity });
+        }));
 }
